1adb58f5b69a101c0efa3e6d6d446a9e3772ef75
uNicom <mokritsa@bk.ru>
2022-07-23 15:24:04 +0300

    add: controller.ts

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,22 +1,24 @@
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: () => void ) {
         super.getResp(
             {
                 endpoint: 'sources',
+                options: Object,
             },
             callback
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: () => void) {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
+                if(!sourceId) throw new Error('Отсутствует data-source-id')
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -29,9 +31,12 @@ class AppController extends AppLoader {
                         callback
                     );
                 }
+                //console.log(target);
                 return;
             }
-            target = target.parentNode;
+            //if (!target.parentNode) throw new ReferenceError('На странице отсутствует target.parentNode');
+            target = target.parentNode as HTMLElement;
+            
         }
     }
 }
